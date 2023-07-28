755d95d1c7ca7958cbb0eb109ba200b0303db3c2
William Murphy <w.williamjmurphy@gmail.com>
2023-07-28 12:15:02 -0600

    finished hello_world

--- hello_world/main.go
+++ hello_world/main.go
@@ -1,7 +1,20 @@
-package main
+// There are executable files and reusable file
 
-import "fmt"
+// (Always at the top)
+package main // Package == Projeect == Workspace
 
+// package name = "main" is used for an executable file
+
+// Any other name other than main
+// means we are making a reusable package
+
+//(Always below package name)
+import "fmt" 
+// -> give my package main all the functionality of package fmt
+
+// func main is needed in package main 
+
+//(Always below the import statements)
 func main() {
     fmt.Println("Hello World!")
 }
