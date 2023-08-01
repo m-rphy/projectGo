a8c11717c8d8a1f2d754b4cb4040f038c231266a
William Murphy <w.williamjmurphy@gmail.com>
2023-08-01 09:24:23 -0600

    adding some testing

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,14 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"math/rand"
-	"os"
-	"strings"
-	"time"
+    "os"
+    "testing"
 )
 
 
+func TestNewDeck(t *testing.T) {
+    d := newDeck()
+    
+    // make sure length is correct
+    if len(d) != 52 {
+        t.Errorf("Expected length of 52, but got %v", len(d))
+    }
+}
 
-;
+// write file to disk
+// (we need to clean up after we're done)
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T){
+    os.Remove("_deckTesting.txt")
+
+    d := newDeck()
+    d.saveToFile("_deckTesting.txt")
+
+    ld := newDeckFromFile("_deckTesting.txt")
+
+    if len(ld) != 52 {
+        t.Errorf("Expected length of 52, got %v", len(ld))
+    }
+
+    os.Remove("_deckTesting.txt")
+}
