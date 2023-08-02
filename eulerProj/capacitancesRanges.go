6a579164c82fe468b0e05947653c4e515bcb72e1
William Murphy <w.williamjmurphy@gmail.com>
2023-08-01 22:49:46 -0600

    refactor capacitance range

--- eulerProj/capacitancesRanges.go
+++ eulerProj/capacitancesRanges.go
@@ -2,70 +2,51 @@ package main
 
 import "fmt"
 
-
-func inParallel(cap []float64) float64 {
+func processComb(comb []int, cap float64) float64 {
     total := 0.0
 
-    for _, c := range cap {
-        total += 1/c
-    }
-    return 1/ total
-}
-
-func inSeries(cap []float64) float64 {
-
-    total := 0.0
-
-    for _, c := range cap {
-        total += c
+    for _, val := range comb {
+        if val == 1 {
+            total += cap
+        } else {
+            total += 1/cap
+        }
     }
     return total
 }
 
-func generateCombinations(cap []float64, n int) [][]float64 {
-    
-    if n == 0 {
-        return [][]float64{{}}
+// Function to generate all possible combinations of capacitors
+func generateCombinations(n int, arr []int, i int) []int {
+
+    comb := arr
+    // basecase:
+    if i == n {
+        return comb
     }
     
-    if len(cap) == 0 {
-		return nil
-	}
+    comb[i] = 0
+    comb = generateCombinations(n, comb, i+1)
 
-	first, rest := cap[0], cap[1:]
 
-	var combinations [][]float64
-
-	for _, sub := range generateCombinations(rest, n-1) {
-		combinations = append(combinations, append([]float64{first}, sub...))
-	}
-    fmt.Print(combinations)
-	combinations = append(combinations, generateCombinations(rest, n)...)
-
-	return combinations
-
+    comb[i] = 1
+    comb = generateCombinations(n, comb, i+1)
+    
+    return comb
 }
 
 func main() {
 	// Define the capacitance of each individual capacitor
-	capacitance := 60.0 // Replace this with the capacitance value in Farads
+	cap := 60.0 // Replace this with the capacitance value in Farads
 
 	n := 3 // Replace this with the number of identical capacitors
 
-	// Generate all possible combinations of capacitors
-	capacitances := make([]float64, n)
-	for i := 0; i < n; i++ {
-		capacitances[i] = capacitance
-	}
-    
-	allCombinations := generateCombinations(capacitances, n)
+    var arr []int
+    comb := generateCombinations(n, arr, 0)
 
-	// Calculate and print all possible capacitances
-	fmt.Printf("All possible capacitances of %d identical capacitors with capacitance %f Farads:\n", n, capacitance)
-	for _, comb := range allCombinations {
-		seriesCap := inSeries(comb)
-		parallelCap := inParallel(comb)
-		fmt.Printf("Series: %f Farads, Parallel: %f Farads\n", seriesCap, parallelCap)
-        fmt.Println(allCombinations)
-	}
+    for i := 0; i<=n; i++ {
+        c := processComb(comb, cap)
+        fmt.Print(c)
+
+    }
+
 }
