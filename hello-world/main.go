81dc22028950d57241f9c418d254fbf6f1478ed1
William Murphy <w.williamjmurphy@gmail.com>
2023-08-09 16:42:37 -0600

    exploring multiple end points

--- hello-world/main.go
+++ hello-world/main.go
@@ -5,17 +5,34 @@ import (
 	"net/http"
 )
 
+const PORT = ":8080"
+
+// Uppercase function makes it public!
+// Home is th home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the Home Page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the About Page and 2 + 2 is %d", sum))
+}
+
+// lower case func name makes the function private!
+// addValues add two integer and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
 // The basis for any web application in Go!
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World")
-		if err != nil {
-			fmt.Println(err)
-		}
+	// Handler functions for different endpoints
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		fmt.Println(fmt.Sprintf("Bytes written: %d", n))
-	})
+	fmt.Println(fmt.Sprintf("Beep Boop, starting on port%s", PORT))
 
-	_ = http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(PORT, nil)
 }
